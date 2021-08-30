2675c293afaa27a115031d3482bdb17d3ea43367
Aiden S <UnaR0219@protonmail.com>
2021-08-30 09:09:54 -0400

    ..

--- baekjoon/baekjoon_11399.cpp
+++ baekjoon/baekjoon_11399.cpp
@@ -17,21 +17,5 @@ int main(){
 		sum += arr[i];
 	}
 	cout << sum << endl;
-	
-	
-	
-	/*
-	cin >> N;
-	int count = 0;	//count
-	while (N >= 0){
-		if (N % 5 == 0){	//check if %5 = 0, 
-			count += N/5;
-			cout << count << endl;
-			return 0;
-		}
-		N = N - 3;	//if it cant be divided by 5, then -3kg
-		count++;
-	}	
-	cout << -1 << endl;
-	*/
+	return 0;
 }
