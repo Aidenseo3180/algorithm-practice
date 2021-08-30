7e5e8bb4eeda6d44a95d40e95bbb8a5c19fac36b
Aiden S <UnaR0219@protonmail.com>
2021-08-30 09:25:18 -0400

    Add files via upload

--- baekjoon/baekjoon_11399.cpp
+++ baekjoon/baekjoon_11399.cpp
@@ -1,7 +1,6 @@
 //problem : https://www.acmicpc.net/problem/11399
 #include <iostream>
 #include <bits/stdc++.h>
-#include <vector>
 
 using namespace std;
 
@@ -11,21 +10,19 @@ int main(){
 	cin >> N;
 	int arr[N];
 	int sum = 0;
+	
 	for (int i = 0; i < N; i++){
 		cin >> arr[i];
 	}
 	
 	int nn = sizeof(arr)/sizeof(arr[0]);
+	
 	sort(arr, arr+nn);
 	for(int j = 0; j < N; j++){
 		arr[j] = arr[j] + arr[j-1];
 		sum += arr[j];
-	}
-	
+	}	
 	cout << sum << endl;
 
 	return 0;
 }
-
-
-
