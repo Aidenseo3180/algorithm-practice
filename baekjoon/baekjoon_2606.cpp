a89387531e3f015a6c68deacd246d58dbc8ed629
Aiden Seo <UnaR0219@protonmail.com>
2022-05-05 14:32:25 -0400

    Add files via upload

---
+++ baekjoon/baekjoon_2606.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+int main(){
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	//N : number of computers, M : numbr of connections
+	std::cin >> N >> M;	
+	
+	for (int i = 0; i < M; i++){
+		
+		
+		
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
