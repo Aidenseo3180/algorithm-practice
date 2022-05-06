b37fddd4a24a877d42bb2fefaa833dccb518c8ce
Aiden Seo <UnaR0219@protonmail.com>
2022-05-06 14:20:38 -0400

    Add files via upload

---
+++ baekjoon/baekjoon_11727.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int arr[1001];
+
+int dp(int x){
+	if (x == 1) return 1;
+	if (x == 2) return 3;	
+	if (arr[x] != 0) return arr[x];
+	
+	return arr[x] = (dp(x-1) + 2*dp(x-2)) % 10007;
+	
+}
+
+int main() {
+    int x;
+	scanf("%d", &x);
+	printf("%d", dp(x));
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
