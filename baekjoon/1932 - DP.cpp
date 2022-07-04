a758e66072eb02b0e8c87181f92bc86eb6c443c5
Aiden Seo <UnaR0219@protonmail.com>
2022-07-04 10:40:41 -0400

    Add files via upload

--- baekjoon/1932 - DP.cpp
+++ baekjoon/1932 - DP.cpp
@@ -16,22 +16,24 @@ int main(){
 	int n;
 	cin >> n; 
 	
+	//input
 	for(int i = 1; i <= n; i++)
 		for (int j = 1; j <= i; j++)
 			cin >> arr[i][j];
 
 	
+	//bottom-up
 	dp[1][1] = arr[1][1];
 	
-	for (int i = 2; i <= n; i++){
-		for(int j = 1; j <= i - 1; j++){			
+	for (int i = 2; i <= n; i++){	//when i = 2, j also goes to 2. But since we're calculating both i,j and i,j+1, j goes until i -1
+		for(int j = 1; j <= i - 1; j++){
 			dp[i][j] = max(dp[i][j], dp[i - 1][j] + arr[i][j]);
 			dp[i][j + 1] = max(dp[i][j + 1], dp[i - 1][j] + arr[i][j + 1]);
 		}
 	}
 	
 	int max_val = -1e9;
-	for (int i = 1; i <= n; i++){
+	for (int i = 1; i <= n; i++){	//find max from the end of the array n
 		if (max_val < dp[n][i]) max_val = dp[n][i];
 	}
 	
